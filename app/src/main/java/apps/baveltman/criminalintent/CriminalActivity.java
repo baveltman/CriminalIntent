67c7e5103c010d0445b059c08b8f276a59f5db88
baveltman <baveltman@gmail.com>
2015-02-28 18:38:20 -0600

    Setting the framework for using fragments and created a model class to encapsulate Crimes

--- app/src/main/java/apps/baveltman/criminalintent/CriminalActivity.java
+++ app/src/main/java/apps/baveltman/criminalintent/CriminalActivity.java
@@ -1,12 +1,10 @@
 package apps.baveltman.criminalintent;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class CriminalActivity extends ActionBarActivity {
+public class CriminalActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +12,4 @@ public class CriminalActivity extends ActionBarActivity {
         setContentView(R.layout.activity_crime);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_criminal, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
