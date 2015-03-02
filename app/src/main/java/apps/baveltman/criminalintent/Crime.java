697715956c0d40f39ad99fab915b60d98918e580
baveltman <baveltman@gmail.com>
2015-03-01 18:40:53 -0600

    Strated building the CrimeListFragment with an adapter and ArrayList<Crime>

--- app/src/main/java/apps/baveltman/criminalintent/Crime.java
+++ app/src/main/java/apps/baveltman/criminalintent/Crime.java
@@ -47,4 +47,9 @@ public class Crime {
     public void setSolved(boolean solved) {
         mSolved = solved;
     }
+
+    @Override
+    public String toString(){
+        return mTitle;
+    }
 }
--- app/src/main/java/apps/baveltman/criminalintent/CrimeListFragment.java
+++ app/src/main/java/apps/baveltman/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package apps.baveltman.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -17,5 +18,13 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceStateBundle);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        //set the adapter for the list
+        ArrayAdapter<Crime> adapter =
+                new ArrayAdapter<Crime>(getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        mCrimes);
+
+        setListAdapter(adapter);
     }
 }
