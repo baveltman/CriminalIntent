9fec17e2b4a0f6c56b206208a492befca1ee6ed8
baveltman <baveltman@gmail.com>
2015-03-01 22:58:31 -0600

    updating ListFragment if a CrimeFragment has been updated

--- app/src/main/java/apps/baveltman/criminalintent/CrimeListFragment.java
+++ app/src/main/java/apps/baveltman/criminalintent/CrimeListFragment.java
@@ -3,7 +3,6 @@ package apps.baveltman.criminalintent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -42,6 +41,12 @@ public class CrimeListFragment extends ListFragment {
         Intent i = new Intent(getActivity(), CriminalActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
     }
 
     /**
