6e1b8f3820ba9c9844a9da88aad5e4231a8a57ec
baveltman <baveltman@gmail.com>
2015-03-07 10:50:15 -0600

    Added empty list view to ListFragment

--- app/src/main/java/apps/baveltman/criminalintent/CrimeListFragment.java
+++ app/src/main/java/apps/baveltman/criminalintent/CrimeListFragment.java
@@ -12,11 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.zip.Inflater;
 
 /**
  * a fragment that is a list UI of crimes
@@ -26,37 +28,67 @@ public class CrimeListFragment extends ListFragment {
 
     private boolean mSubtitleVisible;
     private ArrayList<Crime> mCrimes;
+    private Button mNewCrimeButton;
 
     @Override
     public void onCreate(Bundle savedInstanceStateBundle){
         super.onCreate(savedInstanceStateBundle);
 
+        //activity will have an action bar
         setHasOptionsMenu(true);
-
         getActivity().setTitle(R.string.crimes_title);
 
+        //get the dataset for crimes
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
 
         //set the adapter for the list
         ArrayAdapter<Crime> adapter = new CrimeAdapter(mCrimes);
+        setListAdapter(adapter);
 
         //retain instance vars on configuration change
         setRetainInstance(true);
         mSubtitleVisible = false;
 
-        setListAdapter(adapter);
+
     }
 
     @TargetApi(11)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View v = super.onCreateView(inflater, parent, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.list_item_no_crime, parent, false);
+        ListView view = (ListView)v.findViewById(android.R.id.list);
+        view.setEmptyView(v.findViewById(android.R.id.empty));
+
+        bindEmptyViewUiElements(v);
+        bindNewCrimeButtonEvents();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB && mSubtitleVisible){
             getActivity().getActionBar().setSubtitle(R.string.subtitle);
         }
 
         return v;
+    }
+
+    private void bindEmptyViewUiElements(View v) {
+        mNewCrimeButton = (Button)v.findViewById(R.id.new_crime_button);
+    }
+
+    private void bindNewCrimeButtonEvents() {
+        mNewCrimeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startNewCrimeActivity();
+            }
+        });
+    }
+
+    private void startNewCrimeActivity() {
+        Crime crime = new Crime();
+        CrimeLab.get(getActivity()).addCrime(crime);
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+        i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
+        startActivityForResult(i, 0);
     }
 
     @Override
@@ -75,11 +107,7 @@ public class CrimeListFragment extends ListFragment {
         switch (item.getItemId()) {
 
             case R.id.menu_item_new_crime:
-                Crime crime = new Crime();
-                CrimeLab.get(getActivity()).addCrime(crime);
-                Intent i = new Intent(getActivity(), CrimePagerActivity.class);
-                i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
-                startActivityForResult(i, 0);
+                startNewCrimeActivity();
                 return true;
 
             case R.id.menu_item_show_subtitle:
