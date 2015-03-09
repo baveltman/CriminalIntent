a1f5b605e43a7b30b960e670dcece37acadd2db6
baveltman <baveltman@gmail.com>
2015-03-08 20:43:20 -0500

    Setting up camera surface view in fragment

--- app/src/main/java/apps/baveltman/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/apps/baveltman/criminalintent/CrimeCameraFragment.java
@@ -1,14 +1,21 @@
 package apps.baveltman.criminalintent;
 
 
+import android.annotation.TargetApi;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Controller for the camera view
@@ -39,5 +46,93 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceview);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // setType() and SURFACE_TYPE_PUSH_BUFFERS are both deprecated,
+        // but are required for Camera preview to work on pre-3.0 devices.
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            public void surfaceCreated(SurfaceHolder holder) {
+                // Tell the camera to use this surface as its preview area
+                try {
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if (mCamera == null) return;
+                // The surface has changed size; update the camera preview
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes());
+                parameters.setPreviewSize(s.width, s.height);
+                mCamera.setParameters(parameters);
+
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // We can no longer display on this surface, so stop the preview
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
+
+        });
+    }
+
+    /**
+     * obtains camera resources when user is interacting with the view
+     */
+    @TargetApi(9)
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            mCamera = Camera.open(0);
+        } else {
+            mCamera = Camera.open();
+        }
+    }
+
+    /**
+     * releases camera resources when user no longer interacting with view
+     */
+    @Override
+    public void onPause(){
+        super.onPause();
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    /** A simple algorithm to get the largest size available. For a more
+     * robust version, see CameraPreview.java in the ApiDemos
+     * sample app from Android. */
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes){
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+
+        for (Camera.Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+
+        return bestSize;
     }
 }
